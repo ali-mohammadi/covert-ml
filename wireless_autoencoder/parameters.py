51992b3aac12647aad15314134e701044f537aa9
Ali Mohammadi <ali.mohammaditeshniz@ucalgary.ca>
2022-05-02 00:56:59 -0600

    Add AWGN trained model, data

--- wireless_autoencoder/parameters.py
+++ wireless_autoencoder/parameters.py
@@ -22,20 +22,21 @@ model_parameters = {
 ''' 
     Model parameters for AWGN
 '''
-# model_parameters = {
-#     'n_channel': 8,
-#     'k': 4,
-#     'ebno': 7,
-#     'train_size': 1024 * 8,
-#     'test_size': 1024 * 50,
-#     'batch_size': 32,
-#     'num_epochs': 100,
-#     'learning_rate': 1e-3,
-#     'channel_type': 'awgn',
-#     'channel_samples': None,
-#     'channel_taps': None,
-#     'seed': 0
-# }
+model_parameters = {
+    'n_channel': 8,
+    'k': 4,
+    'ebno': 7,
+    'train_size': 1024 * 8,
+    'test_size': 1024 * 50,
+    'batch_size': 32,
+    'num_epochs': 100,
+    'learning_rate': 1e-3,
+    'channel_type': 'awgn',
+    'channel_samples': None,
+    'channel_taps': None,
+    'seed': 0,
+    'device': 'auto'
+}
 
 
 '''
@@ -54,6 +55,7 @@ model_parameters = {
 #     'channel_samples': 10,
 #     'channel_taps': None,
 #     'seed': 0
+#     'device': 'auto'
 # }
 
 model_parameters['m'] = 2 ** model_parameters['k']
